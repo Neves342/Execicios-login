24a028ccdd23a2f6314f69b84373475f0903f454
Yasmin Neves <minneves63@gmail.com>
2026-09-22 11:25:07 -0300

    altera

--- telas de login/Login2/eye.js
+++ telas de login/Login2/eye.js
@@ -1,12 +1,12 @@
-let senha = document.getElementaryById("senha");
-let icone = document.getElementsByClassName("olho");
+let senha = document.getElementById("senha");
+let icone = document.querySelector(".img-olho");
 
 function mostrar_senha() {
-    if (senha.type != "text") {
+    if (senha.type !== "text") {
         senha.type = "text";
-        console.log(senha.type)
-    }
-    else {
+        console.log(senha.type);
+    }else {
         senha.type = "password";
+       
     }
 }
